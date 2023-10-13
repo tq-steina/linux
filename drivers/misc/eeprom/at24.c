aee334f182e0fb0c1bd3f36bff50f5eb2bd65129
Alexander Stein <alexander.stein@ew.tq-group.com>
2023-10-13 08:30:08 +0200

    eeprom: at24: add ST M24C64-D Additional Write lockable page support
    The ST M24C64-D behaves as a regular M24C64, except for the -D variant
    which uses up another I2C address for Additional Write lockable page.
    This page is 32 Bytes long and can contain additional data. Add entry
    for it, so users can describe that page in DT. Note that users still
    have to describe the main M24C64 area separately as that is on separate
    I2C address from this page.
    
    Signed-off-by: Alexander Stein <alexander.stein@ew.tq-group.com>
    Reviewed-by: Marek Vasut <marex@denx.de>
    Signed-off-by: Bartosz Golaszewski <bartosz.golaszewski@linaro.org>
    (cherry picked from commit 3774740fb22162d2c50e79629c4b3e11022ed7d9)
    Signed-off-by: Alexander Stein <alexander.stein@ew.tq-group.com>

--- drivers/misc/eeprom/at24.c
+++ drivers/misc/eeprom/at24.c
@@ -194,6 +194,8 @@ AT24_CHIP_DATA(at24_data_24c32, 32768 / 8, AT24_FLAG_ADDR16);
 AT24_CHIP_DATA(at24_data_24cs32, 16,
 	AT24_FLAG_ADDR16 | AT24_FLAG_SERIAL | AT24_FLAG_READONLY);
 AT24_CHIP_DATA(at24_data_24c64, 65536 / 8, AT24_FLAG_ADDR16);
+/* M24C64-D Additional Write lockable page (M24C64-D order codes) */
+AT24_CHIP_DATA(at24_data_24c64d_wlp, 32, AT24_FLAG_ADDR16);
 AT24_CHIP_DATA(at24_data_24cs64, 16,
 	AT24_FLAG_ADDR16 | AT24_FLAG_SERIAL | AT24_FLAG_READONLY);
 AT24_CHIP_DATA(at24_data_24c128, 131072 / 8, AT24_FLAG_ADDR16);
@@ -224,6 +226,7 @@ static const struct i2c_device_id at24_ids[] = {
 	{ "24c32",	(kernel_ulong_t)&at24_data_24c32 },
 	{ "24cs32",	(kernel_ulong_t)&at24_data_24cs32 },
 	{ "24c64",	(kernel_ulong_t)&at24_data_24c64 },
+	{ "24c64-wl",	(kernel_ulong_t)&at24_data_24c64d_wlp },
 	{ "24cs64",	(kernel_ulong_t)&at24_data_24cs64 },
 	{ "24c128",	(kernel_ulong_t)&at24_data_24c128 },
 	{ "24c256",	(kernel_ulong_t)&at24_data_24c256 },
@@ -254,6 +257,7 @@ static const struct of_device_id at24_of_match[] = {
 	{ .compatible = "atmel,24c32",		.data = &at24_data_24c32 },
 	{ .compatible = "atmel,24cs32",		.data = &at24_data_24cs32 },
 	{ .compatible = "atmel,24c64",		.data = &at24_data_24c64 },
+	{ .compatible = "atmel,24c64d-wl",	.data = &at24_data_24c64d_wlp },
 	{ .compatible = "atmel,24cs64",		.data = &at24_data_24cs64 },
 	{ .compatible = "atmel,24c128",		.data = &at24_data_24c128 },
 	{ .compatible = "atmel,24c256",		.data = &at24_data_24c256 },
